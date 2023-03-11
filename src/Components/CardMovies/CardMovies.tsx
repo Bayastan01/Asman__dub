c22796127e3a67b0537a10cb6962bce8c6d5a40e
Bayastan01 <104631648+Bayastan01@users.noreply.github.com>
2023-03-11 09:32:14 -0800

    vse

--- src/Components/CardMovies/CardMovies.tsx
+++ src/Components/CardMovies/CardMovies.tsx
@@ -1,79 +1,187 @@
+import { useState } from 'react'
 import './CardMovies.css'
-const mac = [
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-	'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
-]
-const macs = [
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-	'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
-]
+
 export default function CardMovies() {
+	const [imgchange, setImgchange] = useState(true)
+	// const changeImage = () => {
+	// 	setImgchange(!changeImage)
+	// }
+	const changeImage = () => setImgchange(value => !value)
 	return (
 		<>
-			<div className='navigation__list'>
-				{mac.map((item, i) => {
-					return (
-						<>
-							<div className='cards' key={i}>
-								<div className='card__one'>
-									<div className='card__image'>
-										<img className='card__img' src={item} />
-									</div>
-									<div className='card__footer'>
-										<h6>10/9</h6>
-										<div
-											className='
-							navigation__button card__play'
-										></div>
-										<h6>1.55</h6>
+			<div>
+				<div className='card__text_name'>Fimls 24</div>
+				<div className='navigation__list'>
+					{mac.map((item, i) => {
+						return (
+							<>
+								<div className='cards' key={i}>
+									<div className='card__one'>
+										<div className='card__image'>
+											<img className='card__img' src={item.image} />
+										</div>
+
+										<div className='card__name__films'>{item.namemovie}</div>
+
+										<div className='card__footer'>
+											<span className='card__text_time'>10/9</span>
+
+											<div
+												className={`
+							navigation__button ${imgchange === true ? 'card__play1' : 'card__play2'} `}
+												onClick={() => changeImage()}
+											></div>
+											<span className='card__text_time'>1.55</span>
+										</div>
 									</div>
 								</div>
-							</div>
-						</>
-					)
-				})}
+							</>
+						)
+					})}
+				</div>
 			</div>
-			<div className='navigation__list'>
-				{macs.map((item, i) => {
-					return (
-						<>
-							<div className='cards' key={i}>
-								<div className='card__one'>
-									<div className='card__image'>
-										<img className='card__img' src={item} />
-									</div>
-									<div className='card__footer'>
-										<h6>10/9</h6>
-										<div
-											className='
-							navigation__button card__play'
-										></div>
-										<h6>1.55</h6>
+			<div>
+				<div className='card__text_name'>Fimls 24</div>
+				<div className='navigation__list'>
+					{macs.map((item, i) => {
+						return (
+							<>
+								<div className='cards' key={i}>
+									<div className='card__one'>
+										<div className='card__image'>
+											<img className='card__img' src={item.image} />
+										</div>
+										<div className='card__name__films'>{item.namemovie}</div>
+										<div className='card__footer'>
+											<span className='card__text_time'>10/9</span>
+
+											<div
+												className={`
+							navigation__button ${imgchange === true ? 'card__play1' : 'card__play2'} `}
+												onClick={() => changeImage()}
+											></div>
+											<span className='card__text_time'>1.55</span>
+										</div>
 									</div>
 								</div>
-							</div>
-						</>
-					)
-				})}
+							</>
+						)
+					})}
+				</div>
 			</div>
 		</>
 	)
 }
+
+const mac = [
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+	{
+		image:
+			'https://play-lh.googleusercontent.com/GBlf-1i5xhkTP2zmNU8yYYXB2-y7WUA2vTjM1gpQMdQIohqM_yYPPqvkKlZYa_0K9fF4Mux3et63ueo3Hws',
+		namemovie: 'Avatar путь воды',
+	},
+]
+const macs = [
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+	{
+		image:
+			'https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/49628551-2381162.jpg',
+		namemovie: 'Один из нас',
+	},
+]
--- src/Components/Navbars/Navbars.tsx
+++ src/Components/Navbars/Navbars.tsx
@@ -1,43 +1,192 @@
-import { useState } from 'react'
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import React, { useState } from 'react'
 import './Navbars.css'
-const TextNavbar = ['Все', 'Сериал', 'Боевик', 'Мультики', 'Фильмы', 'Новенки']
+const filter = createFilterOptions<FilmOptionType>()
+
 export default function Navbars() {
 	const [navcount, setNavcount] = useState(0)
+	const [Logincount, setLogincount] = useState(1)
 	const categoryindex = index => {
 		setNavcount(index)
 		console.log(navcount)
 	}
+	const [value, setValue] = React.useState<FilmOptionType | null>(null)
+	const [open, toggleOpen] = React.useState(false)
+
+	const handleClose = () => {
+		setDialogValue({
+			title: '',
+			year: '',
+		})
+		toggleOpen(false)
+	}
+
+	const [dialogValue, setDialogValue] = React.useState({
+		title: '',
+		year: '',
+	})
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		setValue({
+			title: dialogValue.title,
+			year: parseInt(dialogValue.year, 10),
+		})
+		handleClose()
+	}
+	const ChangLogin = i => {
+		setLogincount(i)
+	}
 	return (
-		<div className='bg' id='bg__color'>
-			<div className='navbar'>
-				<div className='logo_and_categories'>
-					<a target='_blank'>
-						<h1 className='logo'>Dub.Tv</h1>
-					</a>
-					<div>
-						<ul className='categories_name_navbar'>
-							{TextNavbar.map((text, index) => {
+		<>
+			<div className='bg' id='bg__color'>
+				<div className='navbar'>
+					<div className='logo_and_categories'>
+						<a target='_blank'>
+							<h1 className='logo'>Dub.Tv</h1>
+						</a>
+						<div>
+							<React.Fragment>
+								<Autocomplete
+									style={{ backgroundColor: 'white', borderRadius: 10 }}
+									value={value}
+									onChange={(event, newValue) => {
+										if (typeof newValue === 'string') {
+											setTimeout(() => {
+												toggleOpen(true)
+												setDialogValue({
+													title: newValue,
+													year: '',
+												})
+											})
+										} else if (newValue && newValue.inputValue) {
+											toggleOpen(true)
+											setDialogValue({
+												title: newValue.inputValue,
+												year: '',
+											})
+										} else {
+											setValue(newValue)
+										}
+									}}
+									filterOptions={(options, params) => {
+										const filtered = filter(options, params)
+
+										if (params.inputValue !== '') {
+											filtered.push({
+												inputValue: params.inputValue,
+												title: `${params.inputValue}`,
+											})
+										}
+
+										return filtered
+									}}
+									id='free-solo-dialog-demo'
+									options={top100Films}
+									getOptionLabel={option => {
+										// e.g value selected with enter, right from the input
+										if (typeof option === 'string') {
+											return option
+										}
+										if (option.inputValue) {
+											return option.inputValue
+										}
+										return option.title
+									}}
+									selectOnFocus
+									clearOnBlur
+									handleHomeEndKeys
+									renderOption={(props, option) => (
+										<li {...props}>{option.title}</li>
+									)}
+									className='navbar__search'
+									freeSolo
+									renderInput={params => (
+										<TextField {...params} placeholder='Free solo dialog' />
+									)}
+								/>
+							</React.Fragment>
+						</div>
+						<div className='LoginAndSign'>
+							{login.map((item, i) => {
 								return (
 									<>
-										<li className='nav_text_bar'>
-											<button
-												onClick={() => categoryindex(index)}
-												className={` ${
-													navcount === index ? 'navigation__button_active' : ''
-												}
-									`}
-											>
-												{' '}
-												{text}
-											</button>
-										</li>
+										<div
+											className={`Login__and__Sign ${
+												Logincount === i ? 'ActiveLogin' : ''
+											}`}
+											onClick={() => ChangLogin(i)}
+										>
+											{item}
+										</div>
 									</>
 								)
 							})}
-						</ul>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			<div className='name__dub'>Dub.Tv</div>
+			<ul className='categories_name_navbars'>
+				{TextNavbars.map((text, index) => {
+					return (
+						<>
+							<li className='nav_text_bar' id='name'>
+								<button
+									onClick={() => categoryindex(index)}
+									className={` ${
+										navcount === index ? 'navigation__button_active' : ''
+									}
+									`}
+								>
+									{' '}
+									{text}
+								</button>
+							</li>
+						</>
+					)
+				})}
+			</ul>
+		</>
 	)
 }
+
+interface FilmOptionType {
+	inputValue?: string
+	title: string
+	year?: number
+}
+
+const top100Films: readonly FilmOptionType[] = [
+	{ title: 'Один из нас', year: 1994 },
+	{ title: 'Пантера', year: 1972 },
+	{ title: 'Салам', year: 1974 },
+	{ title: 'The Dark Knight', year: 2008 },
+	{ title: '12 Angry Men', year: 1957 },
+	{ title: "Schindler's List", year: 1993 },
+	{ title: 'Pulp Fiction', year: 1994 },
+]
+
+const TextNavbar = ['Все', 'Сериал', 'Боевик', 'Мультики', 'Фильмы', 'Новенки']
+const login = ['Login', 'Sign Up']
+const TextNavbars = [
+	'Все',
+	'Сериал',
+	'Боевик',
+	'Мультики',
+	'Фильмы',
+	'Новенки',
+	'Все',
+	'Сериал',
+	'Боевик',
+	'Мультики',
+	'Фильмы',
+	'Новенки',
+	'Все',
+	'Сериал',
+	'Боевик',
+	'Мультики',
+	'Фильмы',
+	'Новенки',
+]
